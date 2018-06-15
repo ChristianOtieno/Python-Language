783fe437fd784d7e063a362d140609ba6edb82ed
Christian Otieno <cgao273@gmail.com>
2018-06-15 15:49:29 +0300

    python files

---
+++ codeacademy - Learn Python/Passing a list to a function/Passing a list to a function.py
@@ -0,0 +1,10 @@
+#Using a list as an argument in a function
+#is essentially the same as using just a
+#number or string!
+def list_function(x):
+  return x
+
+n = [3, 5, 7]
+print list_function(n)
+
+
---
+++ codeacademy - Learn Python/Strings in functions/strings in function.py
@@ -0,0 +1,5 @@
+n ="Hello"
+def string_function(s):
+    return s + 'wolrd'
+
+print (string_function(n))
