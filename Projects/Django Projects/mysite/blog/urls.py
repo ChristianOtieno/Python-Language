c752ff67303768973d02aa444910a39b5b34b81c
Christian Otieno <cgao273@gmail.com>
2018-07-04 19:01:04 +0300

    Django web development: Taking care of migrations.

--- Projects/Django Projects/mysite/blog/urls.py
+++ Projects/Django Projects/mysite/blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
-from django.views.generic import Listview, Detailview
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 from blog.models import Post
 
-urlpatterns = [path('', Listview.as_view(queryset=Post.objects.all().order_by("-date")[:25], template_name="blog/blog.html")), path('(?P<pk>\d+)', Detailview.as_view(modele = Post, template_name = 'blog/post.html'))]
+urlpatterns = [path('', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25], template_name="blog/blog.html")), path('(?P<pk>\d+)', DetailView.as_view(model = Post, template_name = 'blog/post.html'))]
