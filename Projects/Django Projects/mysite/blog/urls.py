52943f89b0fdeaadd5c4d79453ff15669f74dda6
Christian Otieno <cgao273@gmail.com>
2018-07-04 07:23:48 +0300

    Django web development: views and templates

--- Projects/Django Projects/mysite/blog/urls.py
+++ Projects/Django Projects/mysite/blog/urls.py
@@ -2,4 +2,4 @@ from django.urls import path, include
 from django.views.generic import Listview, Detailview
 from blog.models import Post
 
-urlpatterns = [path('', Listview.as_view(queryset=Post.objects.all().order_by("-date")[:25], template_name="blog/blog.html"))]
+urlpatterns = [path('', Listview.as_view(queryset=Post.objects.all().order_by("-date")[:25], template_name="blog/blog.html")), path('(?P<pk>\d+)', Detailview.as_view(modele = Post, template_name = 'blog/post.html'))]
