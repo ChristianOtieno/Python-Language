3cd7efec0297ed910a910407333030d6ae414221
Christian Otieno <cgao273@gmail.com>
2018-07-06 20:02:50 +0300

    Changes made to path from path('(?P<pk>\d+)', to re_path('(?P<pk>(\d+))' to fix the issue with named groups. and also other additions.

--- Projects/Django-Projects/mysite/blog/urls.py
+++ Projects/Django-Projects/mysite/blog/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
 from blog.models import Post
 
-urlpatterns = [path('', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25], template_name="blog/blog.html")), path('(?P<pk>\d+)', DetailView.as_view(model = Post, template_name = 'blog/post.html'))]
+urlpatterns = [path('', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25], template_name="blog/blog.html")), re_path('(?P<pk>(\d+))', DetailView.as_view(model = Post, template_name = 'blog/post.html'))]
