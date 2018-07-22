ee19bc18e286dd9c2ee91692903163fe3d3cc7b1
Christian Otieno <cgao273@gmail.com>
2018-07-22 21:19:59 +0300

    Additional comments on what the code all does.

--- codeacademy-LearnPython/8. Loops/XV. Counting as you go.py
+++ codeacademy-LearnPython/8. Loops/XV. Counting as you go.py
@@ -1,3 +1,4 @@
+#enumerate works by supplying a corresponding index to each element in the list that you pass it. Each time you go through the loop, index will be one greater, and item will be the next item in the sequence.
 choices = ['pizza', 'pasta', 'salad', 'nachos']
 
 print ('Your choices are:')
