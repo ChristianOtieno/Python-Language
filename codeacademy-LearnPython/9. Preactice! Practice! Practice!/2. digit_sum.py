1155a01685f479dc0c1a0c390e61099921fb4d74
Christian Otieno <cgao273@gmail.com>
2018-08-01 20:56:00 +0300

    digit sum

---
+++ codeacademy-LearnPython/9. Preactice! Practice! Practice!/2. digit_sum.py
@@ -0,0 +1,9 @@
+#function called digit_sum that takes a positive integer n as input and returns the sum of all that number's digits. For example: digit_sum(1234) should return 10 which is 1 + 2 + 3 + 4. (Assuming that the number you are given will always be positive.)
+
+def digit_sum(x):
+    total = 0
+    while x > 0:
+        total += x % 10
+        x = x // 10
+        print x
+    return total
