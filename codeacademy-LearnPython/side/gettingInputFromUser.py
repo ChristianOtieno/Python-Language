7361c6f4e88753b80f2c3bb7d83c7d354f2a3a81
Christian Otieno <cgao273@gmail.com>
2018-08-08 15:45:27 +0300

    Prompting User for a positive number

--- codeacademy-LearnPython/side/gettingInputFromUser.py
+++ codeacademy-LearnPython/side/gettingInputFromUser.py
@@ -1,8 +1,8 @@
 positiveNumber = None
 while type(positiveNumber) is not int:
    try:
-      n = input("Please enter an integer: ")
-      n = int(positiveNumber)
+      positiveNumber = input("Please enter an integer: ")
+      positiveNumber = int(positiveNumber)
       print("You entered: %d" % positiveNumber)
    except ValueError:
       print("%s is not an integer.\n" % positiveNumber)
