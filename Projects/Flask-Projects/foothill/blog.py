cdd4870af9b5de3f33e85cac3c3d00f40b0211ae
Christian Otieno <cgao273@gmail.com>
2018-08-10 23:10:32 +0300

    foothill

--- Projects/Flask-Projects/foothill/blog.py
+++ Projects/Flask-Projects/foothill/blog.py
@@ -1,16 +1,56 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'd35cdc8da1d3399edfe34c07e101edbc'
+
+posts = [
+    {
+        'author': 'Christian Otieno',
+        'title': 'First Blog Post',
+        'content': 'First post content',
+        'date_posted': 'June, 2018'
+    },
+    {
+        'author': 'Christian Otieno',
+        'title': 'Second Blog Post',
+        'content': 'Second post content',
+        'date_posted': 'July, 2018'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
+
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
-if __name__ == "__main__":
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
     app.run(debug=True)
