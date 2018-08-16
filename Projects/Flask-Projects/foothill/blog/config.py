dca8a63ef75ca4571b807e4e0b388fb85ac73846
Christian Otieno <cgao273@gmail.com>
2018-08-16 11:51:18 +0300

    Restructuring using Blueprints

---
+++ Projects/Flask-Projects/foothill/blog/config.py
@@ -0,0 +1,9 @@
+import os
+class Config:
+    SECRET_KEY = 'd35cdc8da1d3399edfe34c07e101edbc'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
--- Projects/Flask-Projects/foothill/run.py
+++ Projects/Flask-Projects/foothill/run.py
@@ -1,4 +1,6 @@
-from blog import app
+from blog import CreateApp
+
+app = CreateApp()
 
 if __name__ == '__main__':
     app.run(debug=True)
