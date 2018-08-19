ab310e4ae269d6fee6e1e14b67689b5568956117
Christian Otieno <cgao273@gmail.com>
2018-08-19 06:41:23 +0300

    lists

---
+++ Projects/Just-For-Fun/lists.py
@@ -0,0 +1,4 @@
+states = ["Abia", "Adamawa", "Anambra", "Akwa Ibom", "Bauchi", "Bayelsa", "Benue", "Borno", "Cross River", "Delta", "Ebonyi", "Enugu", "Edo", "Ekiti", "Gombe", "Imo", "Jigawa", "Kaduna", "Kano", "Katsina", "Kebbi", "Kogi", "Kwara", "Lagos", "Nasarawa", "Niger", "Ogun", "Ondo", "Osun", "Oyo", "Plateau", "Rivers", "Sokoto", "Taraba", "Yobe", "Zamfara"]
+
+for i in reversed(sorted(states, key=len)):
+    print (i)
