3d4c4fac8f8f29995583ba0d9afe8e7d860b0214
Christian Otieno <cgao273@gmail.com>
2018-08-20 22:25:49 +0300

    Area Calculator

--- codeacademy-LearnPython/3. Conditionals-&-ControlFlow/Area Calculator/Area-Calculator.py
+++ codeacademy-LearnPython/3. Conditionals-&-ControlFlow/Area Calculator/Area-Calculator.py
@@ -6,8 +6,16 @@ option = input("Enter C for Circle or T for Triangle:")
 if option == 'C':
   radius = float(input("Enter the radius of circle: "))
   area = 3.14159 * radius * radius
-print ("Area =",area)
-  
+  print ("Area =", area)
+elif option == 'T':
+  base = float(input("Enter the base of the triangle: "))
+  height = float(input("Enter the height of the triangle: "))
+  area = 0.5 * base * height
+  print ("Area =", area)
+else:
+  print ("You've entered an invalid shape.")
+
+print ("Program is exiting.")
   
   
   
