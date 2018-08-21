7e7626ab3bf55a4a352b6687bb79eb8405c7f3c9
Christian Otieno <cgao273@gmail.com>
2018-08-21 19:49:10 +0300

    Number Guess

--- codeacademy-LearnPython/4. Functions/Number Guess/Number-Guess.py
+++ codeacademy-LearnPython/4. Functions/Number Guess/Number-Guess.py
@@ -2,19 +2,31 @@ from random import randint
 from time import sleep
 
 def GetUserGuess():
-    guess = int(input("Enter your guesses: "))
-    return guess
-
+  guess = int(input("Enter your guesses: "))
+  return guess
 def RollDice(NumberOfSides):
-    guess = int(input("Enter your guesses: "))
-    return guess
-
-def RollDice(NumberOfSides):
-  first_roll = randint(1, NumberOfSides)
-  second_roll = randint(1, NumberOfSides)
+  FirstRoll = randint(1, NumberOfSides)
+  SecondRoll = randint(1, NumberOfSides)
   MaxVal = NumberOfSides * 2
-  print ("The maximum number of sides will be %d " % max_val)
+  print ("The maximum number of sides will be: %d " % MaxVal)
   guess = GetUserGuess()
-  if  guess > MaxVal:
-      print ("%d is too large.")
-
+  if guess > MaxVal:
+    print ("%d is too large." % guess)
+  else:
+    print ("Rolling...")
+    sleep(2)
+    print ("The first dice is %d" % FirstRoll)
+    sleep(1)
+    print ("The first dice is %d" % SecondRoll)
+    sleep(1)
+    TotalRoll = FirstRoll + SecondRoll
+    print ("The total is %d" % TotalRoll)
+    print ("Result...")
+    sleep(1)
+    if guess > TotalRoll:
+      print ("You win!")
+    else:
+      print ("You lose!")
+      
+  RollDice(6)
+  
