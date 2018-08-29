cf808b75a881f1cd8187923835445da14187b87c
Christian Otieno <cgao273@gmail.com>
2018-08-29 18:06:45 +0300

    Qualified Practice

--- Andela_Homestudy/QualifiedPractice1.py
+++ Andela_Homestudy/QualifiedPractice1.py
@@ -1,7 +1,6 @@
 # Complete this function to return either
 # "Hello, [name]!" or "Hello there!"
 # based on the input
-
 def say_hello(name):
     if name == input(name):
         print("Hello, {}".format(name))
