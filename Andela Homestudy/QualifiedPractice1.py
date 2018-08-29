44f98114e58a5c052d013b1a982b2af322a7c6f2
Christian Otieno <cgao273@gmail.com>
2018-08-29 10:43:21 +0300

    Bouncing Back!

---
+++ Andela Homestudy/QualifiedPractice1.py
@@ -0,0 +1,14 @@
+# Complete this function to return either
+# "Hello, [name]!" or "Hello there!"
+# based on the input
+
+def say_hello(name):
+  # you can print to STDOUT to debug if you need to:
+  name = input(str("Enter Your Name: "))
+  print("Hello", name)
+  
+  # but to complete the challenge you need to return a value
+  return "Hello", name # TODO: return correct value
+
+  
+  
