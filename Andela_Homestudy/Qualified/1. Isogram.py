c69d490d62ce0e57e37d249bd7e1a7cbb7f5ecc8
Christian Otieno <cgao273@gmail.com>
2018-09-05 01:15:29 +0300

    qualified

--- Andela_Homestudy/Qualified/1. Isogram.py
+++ Andela_Homestudy/Qualified/1. Isogram.py
@@ -1,6 +1,4 @@
 def is_isogram(word):
     return (word, True) if word and len(set(word)) == len(word) else (word, False)
-
-
-is_isogram('assess')  # ('assess', False)
-is_isogram('rhyme')  # ('rhyme', True)
+is_isogram('assess') 
+is_isogram('rhyme')  
