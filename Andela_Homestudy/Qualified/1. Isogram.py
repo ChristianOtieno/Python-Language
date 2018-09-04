08ab998c6e9efea98a35f994c0dca2def34454ac
Christian Otieno <cgao273@gmail.com>
2018-09-05 01:14:55 +0300

    qualified

--- Andela_Homestudy/Qualified/1. Isogram.py
+++ Andela_Homestudy/Qualified/1. Isogram.py
@@ -1,10 +1,6 @@
-def is_isogram(string):
-    if type(string) != str:
-        raise TypeError('Argument should be a string')
-    elif string == " ":
-        return (string, False)
-    else:
-        for i in string:
-            if string.count(i) > 1:
-                return (string, False)
-    return (string, True)
+def is_isogram(word):
+    return (word, True) if word and len(set(word)) == len(word) else (word, False)
+
+
+is_isogram('assess')  # ('assess', False)
+is_isogram('rhyme')  # ('rhyme', True)
