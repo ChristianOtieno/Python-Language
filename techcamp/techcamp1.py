f462fe69ffdb0c83f6cbea2efedda44bbadb3560
ChristianOtieno <cgao273@gmail.com>
2018-11-05 20:08:47 +0300

    techcamp

---
+++ techcamp/techcamp1.py
@@ -0,0 +1,17 @@
+print("Hello Christian")
+
+print(56+74)
+
+x = "Hello World"
+y = "Ohh my name"
+
+a = 47
+b = 123
+
+sum1 = a + b
+sum2 = x + " " + y
+
+average = sum1 /2
+
+print (sum2)
+print(average)
