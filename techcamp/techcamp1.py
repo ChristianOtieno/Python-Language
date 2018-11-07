b4f4e0c0b49848853200a78aa27b517d6d6ea7a6
Christian Otieno <cgao273@gmail.com>
2018-11-08 00:38:25 +0300

    techcamp

--- techcamp/techcamp1.py
+++ techcamp/techcamp1.py
@@ -15,3 +15,15 @@ average = sum1 /2
 
 print (sum2)
 print(average)
+
+x= ["edwin",[20,30,{"age":30},40,35,"Nairobi"]]
+
+sub1 = x[1]
+sub2 = sub1[2]
+#print(sub1)
+#print(sub2)
+
+#print(sub2['age'])
+#print(x[0], x[1] [1], x[2])
+#print(x[0], x[1][2]["age"], x[2])
+#print(x[0],x[1][2]["age"],x[2])
---
+++ techcamp/techcamp2.py
@@ -0,0 +1,52 @@
+# if 365 <= 35:
+#     print("34 is less than 35")
+#     print("still in the True section")
+# else:
+#     print("35 is less than 365")
+#
+# if 2 > 4 or 34 <= 34:
+#     print(True)
+# else:
+#     print(False)
+#
+#
+# if 2 > 4 or 34 == 35:
+#     print(True)
+# else:
+#     print(False)
+#
+# #USING AND
+# #if one condition is false, the entire statement is false
+# if 2 > 4 and 34 <= 35:
+#     print(True)
+# else:
+#     print(False)
+#
+#
+# if 2 > 4 and 34 ==35:
+#     print(True)
+# else:
+#     print(False)
+
+number = input("Enter number: ")
+numInt = int(number)
+
+if numInt <= 40:
+    print("E")
+elif numInt > 40 and numInt <= 50:
+    print("D")
+elif numInt >50 and numInt <=60:
+    print("C")
+elif numInt > 60 and numInt <=70:
+    print("B")
+elif numInt > 70 and numInt <=100:
+    print("A")
+elif numInt < 0 or numInt > 100:
+    print("Please enter a valid number: ")
+    number = print(int(input("Enter number: ")))
+elif
+    number = print(int(input("Enter number")))
+
+# query = input("Do you want to continue, Y/N?")
+# quiz = chr(Query)
+# if quiz == "Y":
