06f49acbd530b088291366d0f98a920bd308d700
Christian Otieno <cgao273@gmail.com>
2018-11-08 20:37:55 +0300

    techcamp3

--- techcamp/techcamp2.py
+++ techcamp/techcamp2.py
@@ -1,52 +1,24 @@
-# if 365 <= 35:
-#     print("34 is less than 35")
-#     print("still in the True section")
-# else:
-#     print("35 is less than 365")
-#
-# if 2 > 4 or 34 <= 34:
-#     print(True)
-# else:
-#     print(False)
-#
-#
-# if 2 > 4 or 34 == 35:
-#     print(True)
-# else:
-#     print(False)
-#
-# #USING AND
-# #if one condition is false, the entire statement is false
-# if 2 > 4 and 34 <= 35:
-#     print(True)
-# else:
-#     print(False)
-#
-#
-# if 2 > 4 and 34 ==35:
-#     print(True)
-# else:
-#     print(False)
-
-number = input("Enter number: ")
-numInt = int(number)
-
-if numInt <= 40:
-    print("E")
-elif numInt > 40 and numInt <= 50:
-    print("D")
-elif numInt >50 and numInt <=60:
-    print("C")
-elif numInt > 60 and numInt <=70:
-    print("B")
-elif numInt > 70 and numInt <=100:
-    print("A")
-elif numInt < 0 or numInt > 100:
-    print("Please enter a valid number: ")
-    number = print(int(input("Enter number: ")))
-elif
-    number = print(int(input("Enter number")))
-
-# query = input("Do you want to continue, Y/N?")
-# quiz = chr(Query)
-# if quiz == "Y":
+#Define a variable x equal to 1
+x = 1
+#Start while loop until x != 1
+while x == 1:
+    #Prompt the user to enter a number
+    number = input("Enter number: ")
+    #Catch an exception, if a number is string keep x as 1
+    #If not a string break from loop
+    try:
+        number1 = int(number)
+        if number1 < 100 and number1 > 0:
+            if number1 >= 0 and number1 <= 40:
+                print("E")
+            elif number1 > 40 and number1 <= 50:
+                print("D")
+            elif number1 > 50 and number1 <= 60:
+                print("C")
+            elif number1 > 60 and number1 <= 70:
+                print("B")
+            else:
+                print("A")
+            break
+    except:
+        print("Please enter a valid number")
---
+++ techcamp/techcamp3.py
@@ -0,0 +1,11 @@
+# for i in range(1,10):
+#     print(i)
+
+list1 = ["apple","cow","three", "dance"]
+#list2 =[]
+for x in range (0,4):
+    list1[x] = list1[x].capitalize()
+    #list2.append(list1[x].capitalize())
+
+print(list1)
+#print(list2)
