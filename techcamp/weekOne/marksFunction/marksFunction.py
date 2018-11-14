1a859c8d0b757e73d57676f138e657442778afda
Christian Otieno <cgao273@gmail.com>
2018-11-14 08:44:07 +0300

    file restructuring

--- techcamp/weekOne/marksFunction/marksFunction.py
+++ techcamp/weekOne/marksFunction/marksFunction.py
@@ -1,8 +1,18 @@
-maths = int(input("Enter maths: "))
-english = int(input("Enter English: "))
-kiswahili = int(input("Enter Kiswahili: "))
-science = int(input("Enter Science: "))
-social = int(input("Enter Social: "))
+# Define a variable x equal to 1
+x = 1
+# Start while loop until x != 1
+while x == 1:
+    maths = input("Enter maths: ")
+    english = input("Enter English: ")
+    kiswahili = input("Enter Kiswahili: ")
+    science = input("Enter Science: ")
+    social = input("Enter Social: ")
+
+    try:
+        marks = int(maths, english, kiswahili, science, social)
+
+    except:
+        print("Please enter a valid marks")
 
 
 def calculateTotalMarks(maths, english, kiswahili, science, social):
@@ -28,6 +38,6 @@ def calculateGrade(average):
         print("Grade is A")
 
 
-calculateTotalMarks(maths, english, kiswahili, science, social)
-calculateAverageMarks(calculateTotalMarks)
-calculateGrade(calculateAverageMarks)
+x = calculateTotalMarks(maths, english, kiswahili, science, social)
+y = calculateAverageMarks(x)
+z = calculateGrade(y)
---
+++ techcamp/weektwo/taxCalculator/README.md
@@ -0,0 +1,21 @@
+# TAX CALCULATOR
+
+1. Create a program that calculates and prints the net salary of an individual.
+2. The program should take the "basicSalary", "overtime", "houseAllowance", "otherBenefits" and.
+3. Use to calculate the net salary.
+4. Use the Payee Table to find Payee, NHIF Table to find nhifContribution and nssfContribution
+
+*HINT:*
+>netSalary = grossSalary - deductions + personalRelief
+>grossSalary = basicSalary + overtime + huseAllowance + otherBenefits
+>deductions = payee + nhifContribution + nssfContribution
+
+## Functions
+
+i. getNetSalary(gross, deductions)
+ii. getGrossSalary(basic, benefits)
+iii. getDeductions(payee, nhif, nssf)
+iv. getBenefits(overtime, house, other)
+v. getPayee(gross)
+vi. getNHIF(gross)
+vii. getNSSF(gross)
